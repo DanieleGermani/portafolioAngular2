e965167f995ea847d67af7b5b42830d6353f086b
Daniele <daniele.germani.85@gmail.com>
2017-09-19 18:41:40 +0200

    portafolio-item component create

---
+++ src/app/components/portfolio-item/portfolio-item.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item',
+  templateUrl: './portfolio-item.component.html'
+
+})
+export class PortfolioItemComponent {
+
+  constructor() { }
+
+
+
+}
